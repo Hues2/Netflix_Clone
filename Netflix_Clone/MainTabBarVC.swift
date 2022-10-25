585034813a29a0d0d52cb3de54755b5dc983bde3
Greg Ross <greg.ross@networldsports.com>
2022-10-25 11:18:51 +0100

    first commit

---
+++ Netflix_Clone/MainTabBarVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarVC.swift
+//  Netflix_Clone
+//
+//  Created by Greg Ross on 25/10/2022.
+//
+
+import UIKit
+
+class MainTabBarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
